26b62c219991149640f5055af2203bc934d87e70
aB-temps <187414947+aB-temps@users.noreply.github.com>
2025-09-14 18:01:49 +0200

    ✨ feat(fish): alias

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ alias n="nvim"
 #=========== CODE
 alias comp="cc -Wall -Werror -Wextra"
 alias vlg="valgrind --leak-check=full --trace-children=yes --track-fds=yes"
+alias vlgp="vlg --log-socket=127.0.0.1:5454"
 
 #=========== GIT
 alias ga="git add"
@@ -12,6 +13,8 @@ alias gp="git push"
 alias gst="git status"
 alias gsw="git switch"
 alias gb="git branch"
+alias gcl="git clone"
+alias grs="git restore"
 
 #=========== SCRIPTS
 alias cmt="/sgoinfre/abetemps/Public/commit_maker.sh"
