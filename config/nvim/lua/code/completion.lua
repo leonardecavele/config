c12877edfcc8c0018a373954158032f03e540acc
leonardecavele <leonard.decavele@gmail.com>
2026-02-09 20:41:55 +0000

    new: toggle completion binding

--- config/nvim/lua/code/completion.lua
+++ config/nvim/lua/code/completion.lua
@@ -1,5 +1,21 @@
 -- https://github.com/nvim-mini/mini.completion
 local mini_ok, mini_completion = pcall(require, "mini.completion")
 if mini_ok then
-    mini_completion.setup()
+  mini_completion.setup()
+
+  -- Toggle mini.completion ON/OFF
+  vim.g.mini_completion_enabled = true
+
+  vim.keymap.set('n', '<leader>tc', function()
+    vim.g.mini_completion_enabled = not vim.g.mini_completion_enabled
+
+    if vim.g.mini_completion_enabled then
+      pcall(vim.api.nvim_del_augroup_by_name, "MiniCompletion")
+      mini_completion.setup()
+      print("mini.completion: ON")
+    else
+      pcall(vim.api.nvim_del_augroup_by_name, "MiniCompletion")
+      print("mini.completion: OFF")
+    end
+  end, { desc = "Toggle mini.completion" })
 end
