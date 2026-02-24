86f095380930edc5a9bd65b5c602d18aaf1a54c6
leonardecavele <leonard.decavele@gmail.com>
2026-02-24 12:50:44 +0100

    edit: better implementation of lastplace

--- config/nvim/lua/navigation/lastplace.lua
+++ config/nvim/lua/navigation/lastplace.lua
@@ -1,6 +1,9 @@
 -- https://github.com/farmergreg/vim-lastplace
-require("lastplace").setup({
-  lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
-  lastplace_ignore_filetype = { "gitcommit", "gitrebase" },
-  lastplace_open_folds = true,
-})
+local lastplace_ok, lastplace = pcall(require, 'lastplace')
+if lastplace_ok then
+  lastplace.setup({
+    lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+    lastplace_ignore_filetype = { "gitcommit", "gitrebase" },
+    lastplace_open_folds = true,
+  })
+end
