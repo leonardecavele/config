912cfe58dfa5fd90b3db30d5462cb04e829ed23a
leonardecavele <leonard.decavele@gmail.com>
2026-03-15 19:31:43 +0100

    new: fresh restart

--- config/nvim/lua/navigation/lastplace.lua
+++
@@ -1,9 +0,0 @@
--- https://github.com/farmergreg/vim-lastplace
-local lastplace_ok, lastplace = pcall(require, 'lastplace')
-if lastplace_ok then
-  lastplace.setup({
-    lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
-    lastplace_ignore_filetype = { "gitcommit", "gitrebase" },
-    lastplace_open_folds = true,
-  })
-end
